c316796eaee492431c8087704674f2c98931bced
analniy deboshir <hobu4ok.5555@gmail.com>
2022-08-03 22:23:16 +0300

    1

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,27 +5,8 @@ import {NavLink} from "react-router-dom";
 const Header = (props) => {
 
     const urlId = localStorage.getItem('userLogin');
-    const authorized = localStorage.getItem('authorized');
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
 
-    function visible(){
-        document.getElementById("auth").style.visibility = "visible";
-    }
-    function hidden(){
-        document.getElementById("auth").style.visibility = "hidden";
-    }
-
-    function checkAuthorized()
-    {
-        if (authorized === 'true')
-        {
-            hidden();
-        }
-        else
-        {
-            visible();
-        }
-    }
-    setTimeout(checkAuthorized, 0)
     return (
         <header className={s.header}>
             <nav className={s.nav}>
@@ -33,7 +14,7 @@ const Header = (props) => {
                 <NavLink className={s.a} activeClassName={s.activeLink} to={`/messages?id=${urlId}`}>Messages</NavLink>
                 <NavLink className={s.a} activeClassName={s.activeLink} to="/news">News</NavLink>
                 <NavLink className={s.a} activeClassName={s.activeLink} to={`/music?id=${urlId}`}>Music</NavLink>
-                {props.isLoggedIn ? (
+                {isLoggedIn ? (
                     <button className={s.a} activeClassName={s.activeLink} onClick={props.logOut}>Logout</button>
                 ) : (
                     <NavLink id="auth" className={s.a} activeClassName={s.activeLink} to="/authorization">Log In / Sign In</NavLink>
